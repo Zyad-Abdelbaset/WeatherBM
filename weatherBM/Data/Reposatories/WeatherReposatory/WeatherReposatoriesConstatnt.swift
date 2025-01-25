28bdfe8d892c12fcb5beb036e067d90cede96f79
Zyad Abdelbaset <zyad121216@gmail.com>
2025-01-25 14:06:12 +0200

    adding Constatnt of WeatherRepo

---
+++ weatherBM/Data/Reposatories/WeatherReposatory/WeatherReposatoriesConstatnt.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherReposatoriesConstatnt.swift
+//  weatherBM
+//
+//  Created by zyad baset on 23/01/2025.
+//
+
+import Foundation
+extension WeatherReposatories{
+    struct WeatherReposatoriesConstatnt {
+        let aqi:(name:String,value:String) = ("aqi","yes")
+        let days:(name:String,value:String) = ("days","3")
+        let alerts:(name:String,value:String) = ("alerts","no")
+    }
+}
